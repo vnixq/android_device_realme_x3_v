749f17c01398b9a87a724337e5820f74d127120e
Vansh Sethi <156622876+vnixq@users.noreply.github.com>
2024-12-21 04:49:29 +0530

    Update and rename pixelage_x3.mk to lineage_x3.mk

--- lineage_x3.mk
+++ lineage_x3.mk
@@ -20,13 +20,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/realme/x3/device.mk)
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_MINI_GAPPS := true
+EVO_BUILD_TYPE := hotChocolate
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier
-PRODUCT_NAME := pixelage_x3
+PRODUCT_NAME := lineage_x3
 PRODUCT_DEVICE := x3
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme X3 / SuperZoom

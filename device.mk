4934780044c98d0a5e9441ec9940aaeb2e502a04
Vansh Sethi <156622876+vnixq@users.noreply.github.com>
2024-12-22 01:04:53 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -276,7 +276,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pixelage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \

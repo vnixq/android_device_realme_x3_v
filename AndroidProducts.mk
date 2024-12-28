7b4f70c8c33905d6728d38141cb5418d7a31358a
Vansh Sethi <156622876+vnixq@users.noreply.github.com>
2024-12-28 15:41:58 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixelage_x3.mk
+    $(LOCAL_DIR)/lineage_x3.mk
 
 COMMON_LUNCH_CHOICES := \
-    pixelage_x3-ap4a-user \
-    pixelage_x3-ap4a-userdebug \
-    pixelage_x3-ap4a-eng
+    lineage_x3-ap4a-user \
+    lineage_x3-ap4a-userdebug \
+    lineage_x3-ap4a-eng
